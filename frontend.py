fcd3e290a62bc95c1e9c4fbacbfb088af04f4043
Denes Fekete <denes710@gmail.com>
2021-12-08 20:10:06 +0100

    progress: frontend cli usage implemented

---
+++ frontend.py
@@ -0,0 +1,13 @@
+import argparse
+import sys
+
+from consensus_visualizer import ConsensusVisualizer
+from PyQt5.QtWidgets import QApplication
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Program args")
+    parser.add_argument("--input-file", dest="input_file", type=str, help="Json input file")
+    args = parser.parse_args()
+    app = QApplication(sys.argv)
+    ex = ConsensusVisualizer(args.input_file)
+    sys.exit(app.exec_())  
